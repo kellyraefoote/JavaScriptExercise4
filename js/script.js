2618cdcce61bac5866f59a7a79e75cb4aac525ae
Kelly F <95229425+kellyraefoote@users.noreply.github.com>
2022-08-09 18:17:51 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var mainTitle = document.querySelector("h1");
+mainTitle.innerText = "Yay, JavaScript!!";
+console.log(mainTitle);
+
+var body = document.querySelector("body");
+body.style.backgroundColor = "#FEF2F1";
+console.log(body);
